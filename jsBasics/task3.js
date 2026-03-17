db28c0af383b5bf3e54863b932b9e4690fec795e
Vladyslav Synytskyi <cemper51@gmail.com>
2026-03-17 09:14:05 +0100

    DZ_4.1

---
+++ jsBasics/task3.js
@@ -0,0 +1,3 @@
+var age = 17;
+var message = age >= 18 ? 'You are an adult.' : 'You are a minor.';
+console.log(message);
---
+++ jsBasics/task4.js
@@ -0,0 +1,28 @@
+var radius = 5;
+var area = Math.PI * Math.pow(radius, 2);
+console.log('Завдання 4.1');
+console.log('The area of the circle with radius ' + radius + ' is: ' + area.toFixed(2));
+
+///////////////////////////////////
+
+length = 10;
+width = 5;
+area = length * width;
+console.log('Завдання 4.2');
+console.log(
+  'The area of the rectangle with length ' + length + ' and width ' + width + ' is: ' + area,
+);
+
+///////////////////////////////////
+var radius = 5;
+var height = 10;
+var volume = Math.PI * Math.pow(radius, 2) * height;
+console.log('Завдання 4.3');
+console.log(
+  'The volume of the cylinder with radius ' +
+    radius +
+    ' and height ' +
+    height +
+    ' is: ' +
+    volume.toFixed(2),
+);
