d07f93e0bb4f92ead21f6da2de9679faa50fec77
Vladyslav Synytskyi <cemper51@gmail.com>
2026-03-19 18:50:17 +0100

    Edit more ////

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -4,6 +4,7 @@ console.log('Завдання 4.1');
 console.log('The area of the circle with radius ' + radius + ' is: ' + area.toFixed(2));
 
 ///////////////////////////////////
+//////////////////////////////////
 
 length = 10;
 width = 5;
@@ -14,6 +15,8 @@ console.log(
 );
 
 ///////////////////////////////////
+//////////////////////////////////
+
 var radius = 5;
 var height = 10;
 var volume = Math.PI * Math.pow(radius, 2) * height;
