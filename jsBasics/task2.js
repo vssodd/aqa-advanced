b9eda8029614c7dc53852cf2231d0608eefb1c2b
Vladyslav Synytskyi <cemper51@gmail.com>
2026-03-19 20:38:56 +0100

    Add 4 task files

---
+++ jsBasics/task2.js
@@ -0,0 +1,11 @@
+const str = 'Vlad';
+const str2 = 'Synytskyi';
+const str3 = 'AQA';
+
+console.log(str + ' ' + str2 + ' ' + str3);
+
+///////////////////////////////////
+
+const str4 = 'Hello';
+const str5 = `${str4} World`;
+console.log(str5);
