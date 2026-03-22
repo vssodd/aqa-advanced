98cd85a8d3f993cd1a4eebe648193b326bee192d
Vladyslav Synytskyi <cemper51@gmail.com>
2026-03-22 10:19:25 +0100

    Add files

---
+++ DZ_5.1/task2.js
@@ -0,0 +1,20 @@
+const averageGrade = 60;
+
+////////////////////////////////
+
+switch (true) {
+  case averageGrade >= 91:
+    console.log('Відмінно');
+    break;
+  case averageGrade >= 81:
+    console.log('Дуже добре');
+    break;
+  case averageGrade >= 71:
+    console.log('Добре');
+    break;
+  case averageGrade >= 61:
+    console.log('Задовільно');
+    break;
+  default:
+    console.log('Незадовільно');
+}
