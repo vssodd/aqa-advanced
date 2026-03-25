d82e43f6638c3ba54f5e81c83643d4dc9d13bffa
Vladyslav Synytskyi <cemper51@gmail.com>
2026-03-25 22:31:32 +0100

    Add 3x new files

---
+++ task1.js
@@ -0,0 +1,13 @@
+// function areaOfRectangle(length, width) {
+//   return length * width;
+// }
+// console.log(areaOfRectangle(5, 3)); // Output: 15
+
+// const areaOfRectangle = function (length, width) {
+//   return length * width;
+// };
+// console.log(areaOfRectangle(5, 3)); // Output: 15
+
+// const areaOfRectangle = (length, width) => length * width;
+
+// console.log(areaOfRectangle(5, 3)); // Output: 15
