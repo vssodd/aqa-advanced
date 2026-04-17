18de1a1112f72d4acd00b1cec37e069e310319ec
Vladyslav Synytskyi <cemper51@gmail.com>
2026-04-17 20:43:42 +0200

    Add fix to HW

--- task5.js
+++ task5.js
@@ -3,3 +3,7 @@ const users = [
   { name: 'Bob', age: 32 },
   { name: 'Charlie', email: 'charlie@mail.com' },
 ];
+// Updated code for task 5
+for (const { name, email, age } of users) {
+  console.log(name, email ?? 'N/A', age ?? 'N/A');
+}
