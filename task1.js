5d9ff03bd0fb9f32aa97a6ebaafd597e4244cf05
Vladyslav Synytskyi <cemper51@gmail.com>
2026-04-21 17:23:08 +0200

    Clear main

--- task1.js
+++
@@ -1,8 +0,0 @@
-function printWithDelay(text, milliseconds) {
-  setTimeout(() => {
-    console.log(text);
-  }, milliseconds);
-}
-
-printWithDelay('Hello after 1 second', 1000);
-printWithDelay('Hello after 2 seconds', 2000);
